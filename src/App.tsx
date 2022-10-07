0b8e2a6b5fb9a5bbc72883071e47a733762f5a27
ElizPN <pankevych.liza@gmail.com>
2022-10-07 16:59:11 +0200

    create initial template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Button } from "@mui/material";
+import Home from "./onepirate/Home";
 
 function App() {
   return (
     <div className='App'>
       <Button variant='outlined'>Primary</Button>
+      <Home></Home>
     </div>
   );
 }
