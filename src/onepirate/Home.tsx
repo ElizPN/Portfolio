c34d627e113a70bc1b8c4a1a6cb706f9687b96c0
ElizPN <pankevych.liza@gmail.com>
2022-10-07 19:06:06 +0200

    clean up

--- src/onepirate/Home.tsx
+++ src/onepirate/Home.tsx
@@ -1,13 +1,11 @@
-import * as React from 'react';
-import ProductCategories from './modules/views/ProductCategories';
-import ProductSmokingHero from './modules/views/ProductSmokingHero';
-import AppFooter from './modules/views/AppFooter';
-import ProductHero from './modules/views/ProductHero';
-import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from './modules/views/ProductHowItWorks';
-import ProductCTA from './modules/views/ProductCTA';
-import AppAppBar from './modules/views/AppAppBar';
-import withRoot from './modules/withRoot';
+import * as React from "react";
+import ProductCategories from "./modules/views/ProductCategories";
+import AppFooter from "./modules/views/AppFooter";
+import ProductHero from "./modules/views/ProductHero";
+import ProductValues from "./modules/views/ProductValues";
+import ProductCTA from "./modules/views/ProductCTA";
+import AppAppBar from "./modules/views/AppAppBar";
+import withRoot from "./modules/withRoot";
 
 function Index() {
   return (
@@ -16,9 +14,7 @@ function Index() {
       <ProductHero />
       <ProductValues />
       <ProductCategories />
-      <ProductHowItWorks />
       <ProductCTA />
-      <ProductSmokingHero />
       <AppFooter />
     </React.Fragment>
   );
--- src/onepirate/modules/views/AppAppBar.tsx
+++ src/onepirate/modules/views/AppAppBar.tsx
@@ -1,49 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import AppBar from '../components/AppBar';
-import Toolbar from '../components/Toolbar';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import AppBar from "../components/AppBar";
+import Toolbar from "../components/Toolbar";
 
 const rightLink = {
   fontSize: 16,
-  color: 'common.white',
+  color: "common.white",
   ml: 3,
 };
 
 function AppAppBar() {
   return (
     <div>
-      <AppBar position="fixed">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
+      <AppBar position='fixed'>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
           <Box sx={{ flex: 1 }} />
           <Link
-            variant="h6"
-            underline="none"
-            color="inherit"
-            href="/premium-themes/onepirate/"
+            variant='h6'
+            underline='none'
+            color='inherit'
+            href='/premium-themes/onepirate/'
             sx={{ fontSize: 24 }}
           >
-            {'onepirate'}
+            {"onepirate "}
           </Link>
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-in/"
-              sx={rightLink}
-            >
-              {'Sign In'}
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              href="/premium-themes/onepirate/sign-up/"
-              sx={{ ...rightLink, color: 'secondary.main' }}
-            >
-              {'Sign Up'}
-            </Link>
-          </Box>
+
+          <Box
+            sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}
+          ></Box>
         </Toolbar>
       </AppBar>
       <Toolbar />
