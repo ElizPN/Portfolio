30fc208c910181bdeb91fca4ae977d7f9fbb8dd1
ElizPN <pankevych.liza@gmail.com>
2022-10-07 21:16:15 +0200

    delete forms

--- src/onepirate/Home.tsx
+++ src/onepirate/Home.tsx
@@ -3,7 +3,6 @@ import ProductCategories from "./modules/views/ProductCategories";
 import AppFooter from "./modules/views/AppFooter";
 import ProductHero from "./modules/views/ProductHero";
 import ProductValues from "./modules/views/ProductValues";
-import ProductCTA from "./modules/views/ProductCTA";
 import AppAppBar from "./modules/views/AppAppBar";
 import withRoot from "./modules/withRoot";
 
@@ -14,7 +13,6 @@ function Index() {
       <ProductHero />
       <ProductValues />
       <ProductCategories />
-      <ProductCTA />
       <AppFooter />
     </React.Fragment>
   );
