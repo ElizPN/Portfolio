9a226678114427e3167218332d7af82c7b91908b
ElizPN <pankevych.liza@gmail.com>
2022-10-10 19:27:43 +0200

    add background image , change text

--- src/onepirate/modules/views/ProductHero.tsx
+++ src/onepirate/modules/views/ProductHero.tsx
@@ -1,48 +1,37 @@
-import * as React from 'react';
-import Button from '../components/Button';
-import Typography from '../components/Typography';
-import ProductHeroLayout from './ProductHeroLayout';
+import * as React from "react";
+import Button from "../components/Button";
+import Typography from "../components/Typography";
+import ProductHeroLayout from "./ProductHeroLayout";
+import backgroundImage from "./woman-5653501.jpeg";
 
-const backgroundImage =
-  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
-
-export default function ProductHero() {
+export default function ProductHero(): JSX.Element {
   return (
     <ProductHeroLayout
       sxBackground={{
         backgroundImage: `url(${backgroundImage})`,
-        backgroundColor: '#7fc7d9', // Average color of the background image.
-        backgroundPosition: 'center',
+        backgroundColor: "#7fc7d9", // Average color of the background image.
+        backgroundPosition: "center",
       }}
     >
       {/* Increase the network loading priority of the background image. */}
       <img
-        style={{ display: 'none' }}
+        style={{ display: "none" }}
         src={backgroundImage}
-        alt="increase priority"
+        alt='increase priority'
       />
-      <Typography color="inherit" align="center" variant="h2" marked="center">
-        Upgrade your Sundays
+      <Typography color='inherit' align='center' variant='h2' marked='center'>
+        Front End developer
       </Typography>
       <Typography
-        color="inherit"
-        align="center"
-        variant="h5"
+        color='inherit'
+        align='center'
+        variant='h5'
         sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
       >
-        Enjoy secret offers up to -70% off the best luxury hotels every Sunday.
+        TypeScript, JavaScript, React, Redux, CSS, HTML
       </Typography>
-      <Button
-        color="secondary"
-        variant="contained"
-        size="large"
-        component="a"
-        href="/premium-themes/onepirate/sign-up/"
-        sx={{ minWidth: 200 }}
-      >
-        Register
-      </Button>
-      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+
+      <Typography variant='body2' color='inherit' sx={{ mt: 2 }}>
         Discover the experience
       </Typography>
     </ProductHeroLayout>
