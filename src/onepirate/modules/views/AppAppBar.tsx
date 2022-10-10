917af624a9c71b1c1541fd4580ceed2fd0472ebf
ElizPN <pankevych.liza@gmail.com>
2022-10-10 15:17:38 +0200

    replace menu

--- src/onepirate/modules/views/AppAppBar.tsx
+++ src/onepirate/modules/views/AppAppBar.tsx
@@ -1,32 +1,13 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import AppBar from "../components/AppBar";
-import Toolbar from "../components/Toolbar";
-
-const rightLink = {
-  fontSize: 16,
-  color: "common.white",
-  ml: 3,
-};
+import DrawerAppBar from "./DrawerAppBar";
 
 function AppAppBar() {
   return (
     <div>
       <AppBar position='fixed'>
-        <Toolbar>
-          <Link
-            variant='h6'
-            underline='none'
-            color='inherit'
-            href='/premium-themes/onepirate/'
-            sx={{ fontSize: 24 }}
-          >
-            {"onepirate "}
-          </Link>
-        </Toolbar>
+        <DrawerAppBar></DrawerAppBar>
       </AppBar>
-      <Toolbar />
     </div>
   );
 }
