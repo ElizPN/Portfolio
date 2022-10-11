80ec9de90f21abe2d11f38a1f04533990ff73059
ElizPN <pankevych.liza@gmail.com>
2022-10-11 21:05:39 +0200

    add link to project and image

--- src/onepirate/modules/views/ProductValues.tsx
+++ src/onepirate/modules/views/ProductValues.tsx
@@ -1,86 +1,95 @@
-import * as React from 'react';
-import { Theme } from '@mui/material/styles';
-import { SxProps } from '@mui/system';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Typography from '../components/Typography';
+import * as React from "react";
+import { Theme } from "@mui/material/styles";
+import { SxProps } from "@mui/system";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Typography from "../components/Typography";
+import Link from "@mui/material/Link";
+import firstImg from "./ts-react-redux.jpeg";
 
 const item: SxProps<Theme> = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
   px: 5,
 };
 
 function ProductValues() {
   return (
     <Box
-      component="section"
-      sx={{ display: 'flex', overflow: 'hidden', bgcolor: 'secondary.light' }}
+      component='section'
+      sx={{ display: "flex", overflow: "hidden", bgcolor: "secondary.light" }}
     >
-      <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
+      <Container sx={{ mt: 15, mb: 30, display: "flex", position: "relative" }}>
         <Box
-          component="img"
-          src="/static/themes/onepirate/productCurvyLines.png"
-          alt="curvy lines"
-          sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
+          component='img'
+          src='/static/themes/onepirate/productCurvyLines.png'
+          alt='curvy lines'
+          sx={{ pointerEvents: "none", position: "absolute", top: -180 }}
         />
         <Grid container spacing={5}>
           <Grid item xs={12} md={4}>
             <Box sx={item}>
-              <Box
-                component="img"
-                src="/static/themes/onepirate/productValues1.svg"
-                alt="suitcase"
-                sx={{ height: 55 }}
-              />
-              <Typography variant="h6" sx={{ my: 5 }}>
-                The best luxury hotels
+              <Typography variant='h6' sx={{ my: 5 }}>
+                <Link
+                  href='https://elizpn.github.io/react-typical-challenges/'
+                  sx={{ textDecoration: "none" }}
+                >
+                  <Box
+                    component='img'
+                    src={firstImg}
+                    alt='suitcase'
+                    sx={{ height: 160, width: 300 }}
+                  />
+                  React typicall challenges
+                </Link>
               </Typography>
-              <Typography variant="h5">
-                {
+              <Typography variant='h5'>
+                {/* {
                   'From the latest trendy boutique hotel to the iconic palace with XXL pool'
                 }
                 {
                   ', go for a mini-vacation just a few subway stops away from your home.'
-                }
+                } */}
+                On this website you will see solutions to the most common tasks
+                on React.
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
             <Box sx={item}>
               <Box
-                component="img"
-                src="/static/themes/onepirate/productValues2.svg"
-                alt="graph"
+                component='img'
+                src='/static/themes/onepirate/productValues2.svg'
+                alt='graph'
                 sx={{ height: 55 }}
               />
-              <Typography variant="h6" sx={{ my: 5 }}>
+              <Typography variant='h6' sx={{ my: 5 }}>
                 New experiences
               </Typography>
-              <Typography variant="h5">
+              <Typography variant='h5'>
                 {
-                  'Privatize a pool, take a Japanese bath or wake up in 900m2 of garden… '
+                  "Privatize a pool, take a Japanese bath or wake up in 900m2 of garden… "
                 }
-                {'your Sundays will not be alike.'}
+                {"your Sundays will not be alike."}
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
             <Box sx={item}>
               <Box
-                component="img"
-                src="/static/themes/onepirate/productValues3.svg"
-                alt="clock"
+                component='img'
+                src='/static/themes/onepirate/productValues3.svg'
+                alt='clock'
                 sx={{ height: 55 }}
               />
-              <Typography variant="h6" sx={{ my: 5 }}>
+              <Typography variant='h6' sx={{ my: 5 }}>
                 Exclusive rates
               </Typography>
-              <Typography variant="h5">
-                {'By registering, you will access specially negotiated rates '}
-                {'that you will not find anywhere else.'}
+              <Typography variant='h5'>
+                {"By registering, you will access specially negotiated rates "}
+                {"that you will not find anywhere else."}
               </Typography>
             </Box>
           </Grid>
