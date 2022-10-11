d251710b1057542cb6b6f4766eacf92c67261d10
ElizPN <pankevych.liza@gmail.com>
2022-10-11 18:00:13 +0200

    change text

--- src/onepirate/modules/views/ActionAreaCard.tsx
+++ src/onepirate/modules/views/ActionAreaCard.tsx
@@ -10,16 +10,15 @@ export default function ActionAreaCard() {
       <CardActionArea>
         <CardContent>
           <Typography gutterBottom variant='h5' component='div'>
-            Lizard
+            Hello everyone
           </Typography>
           <Typography variant='body2' color='text.secondary'>
-            Hello everyone, my name is Liza Romanova. I have 2 years experience
-            in Front-end development. I have passed multiple Front-end
-            development courses. During that time I have created over 900
-            commits in 26 repositories, solved over 200 algorithmic challenges
-            in JS, created a game in PhaserJS and the biggest project in my
-            portfolio : a Web blog using React with TypeScript and Contentful
-            API.
+            I am junior front-end developer with 2 years experience in Front-end
+            development. I have passed multiple Front-end development courses.
+            During that time I have created over 900 commits in 26 repositories,
+            solved over 200 algorithmic challenges in JS, created a game in
+            PhaserJS and a Web blog using React with TypeScript, Material UI and
+            Contentful API.
           </Typography>
         </CardContent>
       </CardActionArea>
