b1c48d85f4de3b5944e0895ca59314173d756594
ElizPN <pankevych.liza@gmail.com>
2023-02-21 18:45:22 +0100

    add bio

--- src/onepirate/modules/views/ActionAreaCard.tsx
+++ src/onepirate/modules/views/ActionAreaCard.tsx
@@ -16,12 +16,16 @@ export default function ActionAreaCard() {
             fontSize={20}
             maxWidth={700}
           >
-            I am developer with 2 years experience in Front-end development and
-            I continue learning. I have passed multiple Front-end development
-            courses. During that time I have created over 1000 commits in 26
-            repositories, solved over 200 algorithmic challenges in JS, created
-            a game in PhaserJS and a Web blog using React with TypeScript,
-            Material UI and Contentful API.
+            I am a dedicated front-end developer with two years of experience, a
+            passion for continued learning, and a strong track record of
+            building projects with React, TypeScript, Material UI, and other
+            technologies. I have completed multiple front-end development
+            courses and have created over 1,600 commits across 35 repositories.
+            I have solved over 200 algorithmic challenges in JavaScript. My
+            professional competence includes proficiency in TypeScript,
+            JavaScript, React, Redux, Jest, Cypress, MUI, Contentful CMS, Vue,
+            Phaser, CI/CD pipelines using GitHub workflows, Webpack, HTTP
+            protocol, Git, and Yarn/npm as dependency managers.
           </Typography>
         </CardContent>
       </CardActionArea>
